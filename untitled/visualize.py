04b1537834c24146e33a0cd800ee35859547a879
Stanislav Peck <mooknop@gmail.com>
2013-11-23 21:11:29 -0800

    safety commit

--- untitled/visualize.py
+++ untitled/visualize.py
@@ -10,26 +10,46 @@ class Visualizer(object):
         pygame.init()
         size = (600, 600) #(width, height)
         self.screen = pygame.display.set_mode(size)
+        self.states = [0, 4, 4, 3, 1, 5, 2]
         self.state = 0
+        self.phase = 0
 
     def drawDFA(self, dfa, screen):
         screen.fill((0, 0, 0))
         for x in range(len(dfa)):
-            if self.state == x:
+            if self.states[self.state] == x:
                 pygame.draw.rect(screen, pygame.Color(255, 1, 85), (SPOTS[x][0]*125 - 10, SPOTS[x][1]*125- 10, 120, 120), 0)
-                print self.state
             pygame.draw.rect(screen, pygame.Color(1, 85, 255), (SPOTS[x][0]*125, SPOTS[x][1]*125, 100, 100), 0)
+            font = pygame.font.SysFont("Courier", 25)
+            text = font.render(str(x), 1, (0, 0, 0))
+            screen.blit(text, (SPOTS[x][0]*125, SPOTS[x][1]*125))
 
+        self.drawString("samw","i","segamgee")
+
+    def drawString(self, begin, read, end):
+        color = pygame.Color(255, 255, 255)
+        pygame.draw.rect(self.screen, color, (110, 250, 380, 100), 0)
+
+        font = pygame.font.SysFont("Courier", 17)
+        text = font.render(begin, 1, (0, 0, 0))
+        self.screen.blit(text, (110, 250))
+
+        text = font.render(read, 1, (255, 0, 0))
+        self.screen.blit(text, (110, 290))
+
+        text = font.render(end, 1, (0, 0, 0))
+        self.screen.blit(text, (110, 330))
         pygame.display.update()
 
-    def setState(self, val):
-        self.state = val
+    def visualize(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.state += 1
+
+            self.drawDFA([1, 1, 1, 1, 1, 1, 1], self.screen)
+            pygame.display.flip()
 
 a = Visualizer()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            a.state = 6 - a.state
-    a.drawDFA([1, 1, 1, 1, 1, 1, 1, 1],a.screen)
+a.visualize()
